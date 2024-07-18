a99801707d4206fa3ebf09547613230a3350fdd8
SudhanshuuRanjan <sudhanshuranjan2k18@gmail.com>
2024-07-18 22:16:10 +0530

    fixed name

--- client/src/pages/Sales/InternalCollection.jsx
+++ client/src/pages/Sales/InternalCollection.jsx
@@ -249,28 +249,18 @@ const Alumni = () => {
             />
 
             <Select
-              label='Batch'
-              id='batch'
+              label='Designation'
+              id='designation'
               require={true}
               options={[
-                { name: '2014', value: '2014' },
-                { name: '2015', value: '2015' },
-                { name: '2016', value: '2016' },
-                { name: '2017', value: '2017' },
-                { name: '2018', value: '2018' },
-                { name: '2019', value: '2019' },
-                { name: '2020', value: '2020' },
-                { name: '2021', value: '2021' },
-                { name: '2022', value: '2022' },
-                { name: '2023', value: '2023' },
-                { name: '2024', value: '2024' },
+                { name: 'residents', value: 'residents' },
               ]}
-              reactHookForm={register('batch', {
-                required: 'Batch is required',
+              reactHookForm={register('designation', {
+                required: 'Designation is required',
               })}
               className='bg-gray-950 rounded-lg px-3 py-2 mt-1 w-full text-gray-300'
-              errors={errors.batch}
-              placeholder="Select batch"
+              errors={errors.designation}
+              placeholder="Select Designation"
             />
 
             <Input
