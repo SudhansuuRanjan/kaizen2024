987c58f8fe067819f346e2216051b5da3dd907ce
Sudhanshu Ranjan <77230416+SudhansuuRanjan@users.noreply.github.com>
2024-08-23 00:59:08 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -15,7 +15,7 @@ const app = express();
 const allowedOrigins = [
     'http://localhost:5173',
     'https://kaizenaiims.com',
-    'https://kaizen-admin2024.vercel.app'
+    'https://kaizen-admin2024.pages.dev'
 ];
 
 app.use(cors({
