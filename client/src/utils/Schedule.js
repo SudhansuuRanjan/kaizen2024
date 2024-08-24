13377ec2e8d5e17701b73f7dffeeaa9b81c0912d
SudhansuuRanjan <sudhanshuranjan2k18@gmail.com>
2024-08-24 10:53:34 +0530

    fixed schedule

--- client/src/utils/Schedule.js
+++ client/src/utils/Schedule.js
@@ -1,11 +1,402 @@
 const day1 = {
     regular: [
         {
+            time: '7:00 AM - 8:00 AM',
+            events: [
+                {
+                    name: 'Volleyball Women - Match 1',
+                    id: "1"
+                }
+            ]
+        },
+        {
+            time: '8:00 AM - 9:00 AM',
+            events: [
+                {
+                    name: 'Volleyball Women - Match 2',
+                    id: "1"
+                },
+                {
+                    name: 'Badminton Match 1 & 2',
+                    id: "2"
+                },
+                {
+                    name: 'Table Tennis Match 1 & 2',
+                    id: "3"
+                }
+            ]
+        },
+        {
+            time: '9:00 AM - 10:00 AM',
+            events: [
+                {
+                    name: '100M (M)',
+                    id: "1"
+                },
+                {
+                    name: 'Throwball Match 1',
+                    id: "2"
+                },
+                {
+                    name: 'Badminton Match 3 & 4',
+                    id: "3"
+                },
+                {
+                    name: 'Table Tennis Match 3 & 4',
+                    id: "4"
+                }
+            ]
+        },
+        {
+            time: '10:00 AM - 11:00 AM',
+            events: [
+                {
+                    name: '100M (F)',
+                    id: "1"
+                },
+                {
+                    name: 'Throwball Match 2',
+                    id: "2"
+                },
+                {
+                    name: 'Badminton Match 5 & 6',
+                    id: "3"
+                },
+                {
+                    name: 'Table Tennis Match 5 & 6',
+                    id: "4"
+                },
+                {
+                    name: 'Chess Match 1, 2',
+                    id: "5"
+                }
+            ]
+        },
+        {
+            time: '11:00 AM - 12:00 PM',
+            events: [
+                {
+                    name: 'Badminton Match 7 & 8',
+                    id: "1"
+                },
+                {
+                    name: 'Table Tennis Match 7 & 8',
+                    id: "2"
+                },
+                {
+                    name: 'Chess Match 3',
+                    id: "3"
+                }
+            ]
+        },
+        {
+            time: '12:00 PM - 1:00 PM',
+            events: [
+                {
+                    name: 'Javelin Throw',
+                    id: "1"
+                },
+                {
+                    name: 'Badminton Match 9 & 10',
+                    id: "2"
+                },
+                {
+                    name: 'Table Tennis Match 9 & 10',
+                    id: "3"
+                },
+                {
+                    name: 'Chess Match 4',
+                    id: "4"
+                }
+            ]
+        },
+        {
+            time: '1:00 PM - 2:00 PM',
+            events: [
+                {
+                    name: 'Discus Throw',
+                    id: "1"
+                }
+            ]
+        },
+        {
+            time: '2:00 PM - 3:00 PM',
+            events: [
+                {
+                    name: 'Shotput',
+                    id: "1"
+                }
+            ]
+        },
+        {
+            time: '3:00 PM - 4:00 PM',
+            events: [
+                {
+                    name: 'Badminton Match 11 & 12',
+                    id: "1"
+                },
+                {
+                    name: 'Table Tennis Match 11 & 12',
+                    id: "2"
+                },
+                {
+                    name: 'Chess Semi-finals',
+                    id: "3"
+                }
+            ]
+        },
+        {
+            time: '4:00 PM - 5:00 PM',
+            events: [
+                {
+                    name: '200M (M and F)',
+                    id: "1"
+                },
+                {
+                    name: 'Football Semi-final 1',
+                    id: "2"
+                },
+                {
+                    name: 'Volleyball SF1',
+                    id: "3"
+                },
+                {
+                    name: 'Badminton Match 13 & 14',
+                    id: "4"
+                },
+                {
+                    name: 'Table Tennis Match 13 & 14',
+                    id: "5"
+                }
+            ]
+        },
+        {
+            time: '5:00 PM - 6:00 PM',
+            events: [
+                {
+                    name: '4x100M Relay (Men)',
+                    id: "1"
+                },
+                {
+                    name: 'Football Semi-final 2',
+                    id: "2"
+                },
+                {
+                    name: 'Badminton Match 14 & 15',
+                    id: "3"
+                },
+                {
+                    name: 'Table Tennis Match 14 & 15',
+                    id: "4"
+                },
+                {
+                    name: 'Volleyball SF1',
+                    id: "5"
+                }
+            ]
+        },
+        {
+            time: '6:00 PM - 7:00 PM',
+            events: [
+                {
+                    name: '4x100M Relay (Mixed)',
+                    id: "1"
+                },
+                {
+                    name: 'Penalty Shootout',
+                    id: "2"
+                },
+                {
+                    name: 'Volleyball SF2',
+                    id: "3"
+                }
+            ]
+        },
+        {
+            time: '7:00 PM - 8:00 PM',
+            events: [
+                {
+                    name: 'Penalty Shootout',
+                    id: "1"
+                },
+                {
+                    name: 'Volleyball SF2',
+                    id: "2"
+                }
+            ]
+        }
+    ]
+}
+
+const day2 = {
+    regular: [
+        {
+            time: '8:00 AM - 9:00 AM',
+            events: [
+                {
+                    name: 'Table Tennis F1',
+                    id: "1"
+                }
+            ]
+        },
+        {
+            time: '9:00 AM - 10:00 AM',
+            events: [
+                {
+                    name: '100M (M) Final',
+                    id: "1"
+                }
+            ]
+        },
+        {
+            time: '10:00 AM - 11:00 AM',
+            events: [
+                {
+                    name: 'Table Tennis F2',
+                    id: "2"
+                },
+                {
+                    name: '100M (F) Final',
+                    id: "1"
+                },
+                {
+                    name: 'Table Tennis F3',
+                    id: "2"
+                }
+            ]
+        },
+        {
+            time: '11:00 AM - 12:00 PM',
+            events: [
+                {
+                    name: 'Marathon',
+                    id: "1"
+                },
+                {
+                    name: 'Table Tennis F4',
+                    id: "2"
+                }
+            ]
+        },
+        {
+            time: '12:00 PM - 1:00 PM',
+            events: [
+                {
+                    name: 'Marathon',
+                    id: "1"
+                }
+            ]
+        },
+        {
+            time: '1:00 PM - 2:00 PM',
+            events: [
+                {
+                    name: 'Marathon',
+                    id: "1"
+                }
+            ]
+        },
+        {
+            time: '2:00 PM - 3:00 PM',
+            events: [
+                {
+                    name: 'Marathon',
+                    id: "1"
+                }
+            ]
+        },
+        {
+            time: '3:00 PM - 4:00 PM',
+            events: [
+                {
+                    name: 'Marathon',
+                    id: "1"
+                },
+                {
+                    name: 'Badminton F1',
+                    id: "2"
+                },
+                {
+                    name: 'Chess Semi-finals',
+                    id: "3"
+                }
+            ]
+        },
+        {
+            time: '4:00 PM - 5:00 PM',
+            events: [
+                {
+                    name: '200M (M and F) Finals',
+                    id: "1"
+                },
+                {
+                    name: 'Football Final',
+                    id: "2"
+                },
+                {
+                    name: 'Volley and Throwball Finals',
+                    id: "3"
+                },
+                {
+                    name: 'Badminton F2',
+                    id: "4"
+                }
+            ]
+        },
+        {
+            time: '5:00 PM - 6:00 PM',
+            events: [
+                {
+                    name: '4x100 Relay (Men\'s) Finals',
+                    id: "1"
+                },
+                {
+                    name: 'Football Final',
+                    id: "2"
+                },
+                {
+                    name: 'Volley and Throwball Finals',
+                    id: "3"
+                },
+                {
+                    name: 'Badminton F3',
+                    id: "4"
+                }
+            ]
+        },
+        {
+            time: '6:00 PM - 7:00 PM',
+            events: [
+                {
+                    name: '4x100 Mixed Finals',
+                    id: "1"
+                },
+                {
+                    name: 'Penalty Shootout Final',
+                    id: "2"
+                },
+                {
+                    name: 'Volley and Throwball Finals',
+                    id: "3"
+                },
+                {
+                    name: 'Badminton F4',
+                    id: "4"
+                }
+            ]
+        }
+    ]
+}
+
+const day3 = {
+    regular: [
+        {
             time: '9:00 AM',
             events: [
                 {
                     name: 'Classical Dance-Solo',
-                    venue: 'Auditorium',
+                 
+   venue: 'Auditorium',
                     id: "1"
                 }
             ]
@@ -88,7 +479,7 @@ const day1 = {
     ]
 }
 
-const day2 = {
+const day4 = {
     regular: [
         {
             time: '9:00 AM',
@@ -163,9 +554,7 @@ const day2 = {
     ]
 }
 
-
-
-const day3 = {
+const day5 = {
     regular: [
         {
             time: '9:00 AM',
@@ -250,8 +639,68 @@ const day3 = {
     ]
 }
 
+const day6 = {
+    regular: [
+        {
+            time: '9:00 AM',
+            events: [
+                {
+                    name: 'Battle of Bands',
+                    venue: 'Auditorium',
+                    id: "1"
+                }
+            ]
+        },
+        {
+            time: '10:00 AM',
+            events: [
+                {
+                    name: 'Live nature canvas painting',
+                    venue: 'Admin foyer',
+                    id: "2"
+                },
+                {
+                    name: 'Medathena Elite',
+                    venue: 'LT 1',
+                    id: "3"
+                },
+                {
+                    name: 'Popcorn Please! Quiz',
+                    venue: 'LT 2',
+                    id: "4"
+                }
+            ]
+        },
+        {
+            time: '2:00 PM',
+            events: [
+               
+                {
+                    name: 'General Quiz',
+                    venue: 'LT 2',
+                    id: "5"
+                },
+                {
+                    name: 'Nukkad Natak',
+                    venue: 'Admin foyer',
+                    id: "6"
+                },
+                {
+                    name: 'Hogathon',
+                    venue: 'Intern Mess',
+                    id: "7"
+                },
+                {
+                    name: 'Debate English',
+                    venue: 'LT 3',
+                    id: "8"
+                }
+            ]
+        }
+    ]
+}
 
-const day4 = {
+const day7 = {
     regular: [
         {
             time: '9:00 AM',
@@ -311,4 +760,4 @@ const day4 = {
     ]
 }
 
-export { day1, day2, day3, day4 };
+export { day1, day2, day3, day4, day5, day6, day7 };
